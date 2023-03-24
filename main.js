8f93ef80d80d49b0dab7399abd9db4bda0bd9116
BrunoSx1 <s.bruno.12@hotmail.com>
2023-03-24 19:30:54 -0300

    adicionando as funções Try & Catch

--- main.js
+++ main.js
@@ -6,19 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const followersElement = document.querySelector('#followers');
     const followingElement = document.querySelector('#following');
     const linkElement = document.querySelector('#link');
-
-    fetch('https://api.github.com/users/BrunoSx1')
-    .then(function(res) {
+    
+    try {
+        fetch('https://api.github.com/users/BrunoSx1')
+        .then(function(res) {
         return res.json();
-    })
-    .then(function(json){
-        nameElement.innerText = json.name;
-        usernameElement.innerText = "@" + json.login;
-        avatarElement.setAttribute('src', json.avatar_url);
-        reposElement.innerText = json.public_repos;
-        followersElement.innerText = json.followers
-        followingElement.innerText =  json.following;
-        linkElement.setAttribute('href', json.html_url);
-        linkElement.innerText = 'Ver no Github';
-    })
-})
+        })
+            .then(function(json){
+            nameElement.innerText = json.name;
+            usernameElement.innerText = "@" + json.login;
+            avatarElement.setAttribute('src', json.avatar_url);
+            reposElement.innerText = json.public_repos;
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following;
+            linkElement.setAttribute('href', json.html_url);
+            linkElement.innerText = 'Ver no Github';
+        });
+    } catch (error) {
+        console.error('Erro ao tentar obter dados do Github:', error);
+        }
+});
